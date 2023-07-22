3f233ad68fb8a9dd34187983cc4e8ffe46ab25bf
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2023-07-22 18:37:55 -0500

    cursos base de angular

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
+})
+export class ListComponent {
+  public heroesName: string[] = ['SpiderMan', ' Hulk', 'Thor', ' IronMan'];
+  public deletedHero?: string;
+
+  removeLastHeroe(): void {
+    this.deletedHero = this.heroesName.pop();
+  }
+}
